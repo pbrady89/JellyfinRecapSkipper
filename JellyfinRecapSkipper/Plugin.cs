d4df02b9c981f815524bf7f4ef73c0307eb5b775
Paul Brady <pbrady89.pb@gmail.com>
2026-04-10 23:30:46 +0100

    adding plugin

---
+++ JellyfinRecapSkipper/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Controller;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JellyfinRecapSkipper
+{
+    public class Plugin : BasePlugin<BasePluginConfiguration>
+    {
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+            : base(applicationPaths, xmlSerializer)
+        {
+        }
+
+        public override string Name => "Recap Skipper";
+
+        public override Guid Id => new Guid("a8b6e1c2-d4f3-4e5a-9c7b-2f8d6e1a3b5c");
+
+        public override string Description => "Skips previously on recaps in TV series.";
+    }
+
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddRecapSkipper(this IServiceCollection services)
+        {
+            services.AddSingleton<IMediaSegmentProvider, RecapSegmentProvider>();
+            return services;
+        }
+    }
+}
